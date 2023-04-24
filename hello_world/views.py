62268bce1de6e6b957a44045eeda8ce3f3f123d2
ppaszkowiak <paszkowiakpatrycja@gmail.com>
2023-04-24 21:20:58 +0200

    zmianaxyzx

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "ppaszkowiak"
+moje_imie = "patrycja"
 msg = "Hello World!"
 
 
